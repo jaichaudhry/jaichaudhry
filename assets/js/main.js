3b245e8881304ae69a4a475ecd12523c69947035
Jaycee <jai.chaudhry4@gmail.com>
2016-10-02 18:57:23 +0530

    Adding support to load images only when clicked on modal.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,10 @@ jQuery(document).ready(function($) {
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
     
+    $('#feedModal').on('shown.bs.modal', function() {
+        $(this).find('.modal-body').html('<p style="margin-top: 20px;">Implemented the new design for the feed of Roposo. Used AsyncDisplayKit for the same which helped in increasing the scrolling performance by 10x.</p><div class="item row" style="margin-top: 40px;"><div class="col-md-3 project-wrapper-modal"><div class="img-wrapper clearfix" style="width:250px;height:444.67px"><img class="profile-image img-responsive pull-left" src="assets/images/projects/roposo/roposo13.JPG" alt="roposo1"/></div></div><div class="col-md-3 project-wrapper-modal"><div class="img-wrapper clearfix" style="width:250px;height:444.67px"><img class="profile-image img-responsive pull-left" src="assets/images/projects/roposo/roposo14.JPG" alt="roposo1"/></div></div><div class="col-md-3 project-wrapper-modal"><div class="img-wrapper clearfix" style="width:250px;height:444.67px"><img class="profile-image img-responsive pull-left" src="assets/images/projects/roposo/roposo15.JPG" alt="roposo1"/></div></div></div><!--//item-->');
+    });
+
     $('#trimModal').on('shown.bs.modal', function() {
         $(this).find('.modal-body').html('<iframe id="trimIframe" src="https://marvelapp.com/210b3e8" width="100%" height="600"></iframe><p>Implemented the video trim feature to make it easy for user to trim the video while uploading it to Roposo.</p>');
     });
